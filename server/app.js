62942e00830796192bf5278ee2cad10707c72ae9
Viktor Kotusenko <roxolanus@gmail.com>
2016-05-27 20:08:56 +0300

    finishing error handling

--- server/app.js
+++ server/app.js
@@ -34,7 +34,23 @@ app.use((req, res, next) => {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.render('error', {
+      message: err.message,
+      error: err
+    })
+  })
 }
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.render('error', {
+    message: err.message,
+    error: {}
+  })
+})
 
 module.exports = app
