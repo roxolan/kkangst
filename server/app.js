3e902a14def2937d6f3c9daead026b369be5216e
Viktor Kotusenko <roxolanus@gmail.com>
2016-05-27 16:23:33 +0300

    set views engine, routes, main controller

--- server/app.js
+++ server/app.js
@@ -5,14 +5,20 @@ const logger = require('morgan')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 
+const routes = require('../server/routes/index')
+
 const app = express()
 
-app.use(favicon(path.join(__dirname, '..' + '/public/favicon.ico')))
+app.set('views', path.join(__dirname, '..', 'server', 'views'))
+app.set('view engine', 'pug')
+
+app.use(favicon(path.join(__dirname, '..', 'public/favicon.ico')))
 app.use(logger('dev'))
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
 app.use(express.static(path.join(__dirname, '..', '/public')))
+
+app.use('/', routes)
 
 module.exports = app
---
+++ server/routes/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+const ctrlMain = require('../controllers/main')
+
+router.get('/', ctrlMain.index)
+
+module.exports = router
