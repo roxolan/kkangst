1dc7bb4d3e8f9d91f754b32bf6bd860957eb1bef
Viktor Kotusenko <roxolanus@gmail.com>
2016-05-27 15:36:19 +0300

    add favicon & morgan logger

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
 
 const app = express()
+
+app.use(favicon(path.join(__dirname, '..' + '/public/favicon.ico')))
+app.use(logger('dev'))
 
 module.exports = app
