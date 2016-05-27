5bb79c481c2f4fee617cd7b5055eed6f22032798
Viktor Kotusenko <roxolanus@gmail.com>
2016-05-27 15:51:32 +0300

    add body-parser lib

--- server/app.js
+++ server/app.js
@@ -3,11 +3,14 @@ const path = require('path')
 const favicon = require('serve-favicon')
 const logger = require('morgan')
 const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
 
 const app = express()
 
 app.use(favicon(path.join(__dirname, '..' + '/public/favicon.ico')))
 app.use(logger('dev'))
 app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 module.exports = app
