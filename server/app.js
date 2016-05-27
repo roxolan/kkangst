58f6cc84b91948fd853a57f6d1300cf0387490b5
Viktor Kotusenko <roxolanus@gmail.com>
2016-05-27 15:45:27 +0300

    add cookie-parser dep

--- server/app.js
+++ server/app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const path = require('path')
 const favicon = require('serve-favicon')
 const logger = require('morgan')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 
 app.use(favicon(path.join(__dirname, '..' + '/public/favicon.ico')))
 app.use(logger('dev'))
+app.use(cookieParser())
 
 module.exports = app
