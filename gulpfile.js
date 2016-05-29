ee831795d4b00fb48d755620cc252d4094577749
Viktor Kotusenko <roxolanus@gmail.com>
2016-05-29 13:21:01 +0300

    simplify gulpfile & install gulp utilities

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,27 @@
-const requireDir = require('require-dir')
+const gulp = require('gulp')
+const gutil = require('gulp-util')
+const path = require('path')
 
-requireDir('./lib/gulp/tasks', {recurse: true})
+const clean = require('gulp-clean')
+const cleanCSS = require('gulp-clean-css')
+const concat = require('gulp-concat')
+const less = require('gulp-less')
+
+gulp.task('clean', () => {
+  return gulp.src('public/css/*')
+    .pipe(clean({force: true}))
+})
+
+gulp.task('less', [ 'clean' ], () => {
+  return gulp.src('./client/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'lib', 'less', 'includes')]
+    }))
+    .pipe(concat('app.css'))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('public/css'))
+})
+
+gulp.task('default', [ 'less' ], () => {
+  return gutil.log('Less processed!')
+})
