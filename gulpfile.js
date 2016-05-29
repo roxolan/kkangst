440d7538abc1e1b4771b4c970f2b1a8bf8da3689
Viktor Kotusenko <roxolanus@gmail.com>
2016-05-29 16:06:03 +0300

    factor out gulp configs to a separate file

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require('gulp')
 const gutil = require('gulp-util')
 const path = require('path')
 
+const config = require('./gulp-config.json')
+
 const clean = require('gulp-clean')
 const cleanCSS = require('gulp-clean-css')
 const concat = require('gulp-concat')
@@ -10,33 +12,33 @@ const sourcemaps = require('gulp-sourcemaps')
 const uglify = require('gulp-uglify')
 
 gulp.task('clean', () => {
-  return gulp.src('public/css/*')
+  return gulp.src(config.paths.publicall)
     .pipe(clean({force: true}))
 })
 
 gulp.task('less', [ 'clean' ], () => {
-  return gulp.src('./client/less/**/*.less')
+  return gulp.src(config.paths.appless)
     .pipe(less({
-      paths: [ path.join(__dirname, 'lib', 'less', 'includes')]
+      paths: config.paths.includes
     }))
     .pipe(concat('app.css'))
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('public/css'))
+    .pipe(gulp.dest(config.paths.publiccss))
 })
 
 gulp.task('js-app-concat', [ 'clean' ], () => {
-  return gulp.src('./client/app/**/*.js')
+  return gulp.src(config.paths.appjs)
     .pipe(sourcemaps.init())
       .pipe(concat('app.js'))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./public/js'))
+    .pipe(gulp.dest(config.paths.publicjs))
 })
 
 gulp.task('js-app-uglify', [ 'js-app-concat' ], () => {
-  return gulp.src('./client/app/**/*.js')
+  return gulp.src(config.paths.appjs)
     .pipe(concat('app.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('public/js'))
+    .pipe(gulp.dest(config.paths.publicjs))
 })
 
 gulp.task('default', [ 'less', 'js-app-uglify' ], () => {
