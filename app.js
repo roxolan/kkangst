57882ecc8a7136efc035eb3527597d19759bcf46
Viktor Kotusenko <roxolanus@gmail.com>
2016-05-30 12:27:21 +0300

    serving angular & other JS as local assets

--- app.js
+++ app.js
@@ -21,10 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/angular', express.static(path.join(__dirname, 'node_modules/angular')))
-app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')))
-app.use('/jquery', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
-
+app.use('/assets', express.static(path.join(__dirname, 'node_modules')))
 
 // app.use('/', routes)
 app.use((req, res) => {
