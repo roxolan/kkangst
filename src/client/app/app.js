40467fc4d201332ff4a5870782ebfd4c82e361ae
Viktor Kotusenko <roxolanus@gmail.com>
2016-06-03 15:27:16 +0300

    add app.js

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -1,6 +1,32 @@
+'use strict'
 
-angular.module('kkangst', [])
-  .controller('MainCtrl', [function() {
+angular.module('kkangst', [
+  'ngResource',
+  'ngSanitize',
+  'ngTouch',
+  'ui.router'
+])
+
+var app = angular.module('kkangst')
+  .config(
+    ['$controllerProvider', '$compileProvider', '$filterProvider', '$provide', '$interpolateProvider',
+      function($controllerProvider, $compileProvider, $filterProvider, $provide, $interpolateProvider) {
+        app.controller = $controllerProvider.register
+        app.directive = $compileProvider.directive
+        app.filter = $filterProvider.register
+        app.factory = $provide.factory
+        app.service = $provide.service
+        app.constant = $provide.constant
+        app.value = $provide.value
+
+        $interpolateProvider.startSymbol('::')
+        $interpolateProvider.endSymbol('::')
+
+      }
+    ]
+  )
+
+app.controller('MainCtrl', [function() {
     var vm = this
     vm.title = "Titulus"
   }])
