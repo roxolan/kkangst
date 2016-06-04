c961a4524b204762662bf26059d5310afeef2a08
Viktor Kotusenko <roxolanus@gmail.com>
2016-06-04 13:43:40 +0300

    serve from pubic folder in app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/assets', express.static(path.join(__dirname, 'node_modules')))
 
 // app.use('/', routes)
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, 'src', 'client', 'app', 'index.html'))
+  res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'))
 })
 app.use('/users', users)
 
