d25ba0502fc7980d6866f67e5d7b041345f3d2e6
Viktor Kotusenko <roxolanus@gmail.com>
2016-06-04 13:50:52 +0300

    some legacy changes in src/client/app/controller.js

--- src/client/app/controller.js
+++ src/client/app/controller.js
@@ -1,17 +1,29 @@
 // File: chapter3/controller.js
 
-angular.module('notesApp', [])
+angular.module('kkangst', [])
   .controller('MainCtrl', [function() {
-    var self = this
-    self.items = [
+    var vm = this
+    vm.items = [
       {id: 1, label: 'First', done: false},
       {id: 2, label: 'Second', done: false}
     ]
 
-    self.getDoneClass = function(item) {
+    vm.getDoneClass = function(item) {
       return {
         finished: item.done,
         unfinished: !item.done
       }
     }
+
+    vm.change = function() {
+      vm.username = 'changed'
+      vm.password = 'password'
+    }
+
+    vm.submit = function() {
+      console.log('User clicked submit with ',
+        vm.user)
+    }
+
+
   }]);
