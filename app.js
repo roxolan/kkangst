1ab25979601a391e4ca63aaf531334c683cc763b
Viktor Kotusenko <roxolanus@gmail.com>
2016-06-04 14:44:20 +0300

    fix serving error view after submoving to src/server

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const users = require('./src/server/routes/users')
 
 const app = express()
 
-app.set('views', path.join(__dirname, 'server', 'views'))
+app.set('views', path.join(__dirname, 'src', 'server', 'views'))
 app.set('view engine', 'pug')
 
 app.use(favicon(path.join(__dirname, 'public/favicon.ico')))
