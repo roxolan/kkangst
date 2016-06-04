b924d666633bc2c70d71961c111495d6534cd6eb
Viktor Kotusenko <roxolanus@gmail.com>
2016-06-04 13:26:53 +0300

    initial, basic config.router.js

---
+++ src/client/app/config.router.js
@@ -0,0 +1,12 @@
+angular.module('kkangst')
+  .run([ '$rootScope', '$state', '$stateParams',
+    function ($rootScope, $state, $stateParams) {
+      $rootScope.$state = $state
+      $rootScope.$stateParams = $stateParams
+    }
+  ])
+  .config([ '$stateProvider', '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+      var htmlClass = {}
+    }
+  ])
