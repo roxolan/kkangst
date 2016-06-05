16044c9534b3cebfb71e734a965985edc825fc5c
Viktor Kotusenko <roxolanus@gmail.com>
2016-06-05 14:26:20 +0300

    change name of swig task to something more noble

--- gulp/default.js
+++ gulp/default.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp')
 const gutil = require('gulp-util')
 
-gulp.task('default', [ 'less', 'js-app-uglify', 'swigtest' ], () => {
+gulp.task('default', [ 'less', 'js-app-uglify', 'swig' ], () => {
   return gutil.log('With swig')
 })
