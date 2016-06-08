c56470cab2d42ff348642cb6f4e1542f106650c2
Viktor Kotusenko <roxolanus@gmail.com>
2016-06-08 14:30:15 +0300

    move data to controller in generic-text

--- src/server/controllers/others.js
+++ src/server/controllers/others.js
@@ -1,3 +1,6 @@
 module.exports.about = (req, res) => {
-  res.render('generic-text', { title: 'Про LMS' })
+  res.render('generic-text', {
+    title: 'Про LMS',
+    content: 'LMS було створено для підтримки процесу навчання в kmbs. \n\nЦя система містить усе потрібне для організації навчання'
+  })
 }
