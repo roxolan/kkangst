ce15e9e1f3304d3bbf3ade0e2f18fa26f2866955
Viktor Kotusenko <roxolanus@gmail.com>
2016-06-08 23:05:01 +0300

    add mongoose connection & capture process termination events

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const favicon = require('serve-favicon')
 const logger = require('morgan')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
+require('./src/server/models/db')
 
 const routes = require('./src/server/routes/index')
 const users = require('./src/server/routes/users')
