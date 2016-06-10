864eed9100065c3bd09f4dfb2200fdb7369bdb39
Viktor Kotusenko <roxolanus@gmail.com>
2016-06-10 15:38:14 +0300

    create stub contollers

---
+++ src/api/controllers/courses.js
@@ -0,0 +1,8 @@
+// courses API controller
+
+// methods to be created:
+// -- coursesList
+// -- coursesCreate
+// -- coursesReadOne
+// -- coursesUpdateOne
+// -- coursesDeleteOne
---
+++ src/api/controllers/reviews.js
@@ -0,0 +1,8 @@
+// reviews for courses API controller
+
+// methods to be created:
+
+// -- reviewsCreate
+// -- reviewsReadOne
+// -- reviewsUpdateOne
+// -- reviewsDeleteOne
