b7bf75af9e1a8f1ad2447079219b184bfd8ae39b
Viktor Kotusenko <roxolanus@gmail.com>
2016-06-14 16:57:39 +0300

    fix name of sendJsonResponse & .findById usage

--- src/api/controllers/courses.js
+++ src/api/controllers/courses.js
@@ -3,31 +3,35 @@ const Course = mongoose.model('Course')
 
 // courses API controller
 
-const sendJSONresponse = (res, status, content) => {
+const sendJsonResponse = (res, status, content) => {
   res.status(status)
   res.json(content)
 }
 
 module.exports.coursesList = (req, res) => {
-  sendJSONresponse(res, 200, {'status': 'stub'})
+  sendJsonResponse(res, 200, {'status': 'stub'})
 }
 
 module.exports.coursesCreate = (req, res) => {
-  sendJSONresponse(res, 200, {'status': 'stub'})
+  sendJsonResponse(res, 200, {'status': 'stub'})
 }
 
 module.exports.coursesReadOne = (req, res) => {
-  sendJSONresponse(res, 200, {'status': 'stub'})
+  Course
+    .findById(req.params.courseid)
+    .exec((err, course) => {
+      sendJsonResponse(res, 200, course)
+    })
 }
 
 module.exports.coursesList = (req, res) => {
-  sendJSONresponse(res, 200, {'status': 'stub'})
+  sendJsonResponse(res, 200, {'status': 'stub'})
 }
 
 module.exports.coursesUpdateOne = (req, res) => {
-  sendJSONresponse(res, 200, {'status': 'stub'})
+  sendJsonResponse(res, 200, {'status': 'stub'})
 }
 
 module.exports.coursesDeleteOne = (req, res) => {
-  sendJSONresponse(res, 200, {'status': 'stub'})
+  sendJsonResponse(res, 200, {'status': 'stub'})
 }
--- src/api/controllers/reviews.js
+++ src/api/controllers/reviews.js
@@ -3,23 +3,23 @@ const Course = mongoose.model('Course')
 
 // reviews for courses API controller
 
-const sendJSONresponse = (res, status, content) => {
+const sendJsonResponse = (res, status, content) => {
   res.status(status)
   res.json(content)
 }
 
 module.exports.reviewsCreate = (req, res) => {
-  sendJSONresponse(res, 200, {'status': 'stub'})
+  sendJsonResponse(res, 200, {'status': 'stub'})
 }
 
 module.exports.reviewsReadOne = (req, res) => {
-  sendJSONresponse(res, 200, {'status': 'stub'})
+  sendJsonResponse(res, 200, {'status': 'stub'})
 }
 
 module.exports.reviewsUpdateOne = (req, res) => {
-  sendJSONresponse(res, 200, {'status': 'stub'})
+  sendJsonResponse(res, 200, {'status': 'stub'})
 }
 
 module.exports.reviewsDeleteOne = (req, res) => {
-  sendJSONresponse(res, 200, {'status': 'stub'})
+  sendJsonResponse(res, 200, {'status': 'stub'})
 }
