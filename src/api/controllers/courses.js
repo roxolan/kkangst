c620212fe80e5a739cbde0381849ac2ed6297d56
Viktor Kotusenko <roxolanus@gmail.com>
2016-06-15 20:01:54 +0300

    complete controller for creating a new course

--- src/api/controllers/courses.js
+++ src/api/controllers/courses.js
@@ -12,10 +12,6 @@ module.exports.coursesList = (req, res) => {
   sendJsonResponse(res, 200, {'status': 'stub'})
 }
 
-module.exports.coursesCreate = (req, res) => {
-  sendJsonResponse(res, 200, {'status': 'stub'})
-}
-
 /* GET a course by the id */
 module.exports.coursesReadOne = (req, res) => {
   if (req.params && req.params.courseid) {
@@ -34,12 +30,49 @@ module.exports.coursesReadOne = (req, res) => {
           return
         }
         sendJsonResponse(res, 200, course)
+        console.log('-- reviews for this course: ', course.reviews)
       })
   } else {
     sendJsonResponse(res, 404, {
       'message': 'No courseid in request'
     })
   }
+}
+
+module.exports.coursesCreate = (req, res) => {
+  Course.create({
+    name: req.body.name,
+    address: req.body.address,
+    groups: req.body.groups.split(","),
+    classTimes: [
+      {
+        day: req.body.day1,
+        date: req.body.date1,
+        timing: req.body.timing1,
+        room: req.body.room1
+      },
+      {
+        day: req.body.day2,
+        date: req.body.date2,
+        timing: req.body.timing2,
+        room: req.body.room2
+      },
+      {
+        day: req.body.day3,
+        date: req.body.date3,
+        timing: req.body.timing3,
+        room: req.body.room3
+      }
+    ]
+  }, (err, course) => {
+    if (err) {
+      console.log(err)
+      sendJsonResponse(res, 400, err)
+    } else {
+      console.log(course)
+      sendJsonResponse(res, 201, course)
+    }
+  })
 }
 
 module.exports.coursesList = (req, res) => {
