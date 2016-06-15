cf8676f1227d93e6056d06a120640d6c0bafca5d
Viktor Kotusenko <roxolanus@gmail.com>
2016-06-15 20:09:12 +0300

    .reviewsReadOne - still GET method for a subdocument

--- src/api/controllers/reviews.js
+++ src/api/controllers/reviews.js
@@ -12,8 +12,54 @@ module.exports.reviewsCreate = (req, res) => {
   sendJsonResponse(res, 200, {'status': 'stub'})
 }
 
+/* GET a review from a course by the id */
 module.exports.reviewsReadOne = (req, res) => {
-  sendJsonResponse(res, 200, {'status': 'stub'})
+  console.log('Getting single review')
+  if (req.params && req.params.courseid && req.params.reviewid) {
+    Course
+      .findById(req.params.courseid)
+      .select('name reviews')
+      .exec(
+        (err, course) => {
+          console.log(course)
+          var response, review
+          if (!course) {
+            sendJsonResponse(res, 404, {
+              'message': 'courseid not found'
+            })
+            return
+          } else if (err) {
+            sendJsonResponse(res, 404, err)
+            return
+          }
+          if (course.reviews && course.reviews.length > 0) {
+            review = course.reviews.id(req.params.reviewid)
+            if (!review) {
+              sendJsonResponse(res, 404, {
+                'message': 'reviewid not found'
+              })
+            } else {
+              response = {
+                course: {
+                  name: course.name,
+                  id: req.params.courseid
+                },
+                review: review
+              }
+              sendJsonResponse(res, 200, response)
+            }
+          } else {
+            sendJsonResponse(res, 404, {
+              'message': 'No reviews found'
+            })
+          }
+        }
+      )
+  } else {
+    sendJsonResponse(res, 404, {
+      'message': 'Not found, courseid & reviewid are both required'
+    })
+  }
 }
 
 module.exports.reviewsUpdateOne = (req, res) => {
