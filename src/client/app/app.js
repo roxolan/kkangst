93e9575ad06aeffc699bb438947c2f29dc9bfc42
Viktor Kotusenko <roxolanus@gmail.com>
2016-06-24 15:38:37 +0300

    refactor NG functions setter-getter way & finally managing to output mainCtrl properties

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -9,26 +9,29 @@ var mainCtrl = function () {
   vm.year = 1974
 }
 
+var courseListCtrl = function () {
+  var vm = this
+  vm.courses = [
+    {
+      name: 'Маркетинг',
+      address: 'Київ, вул. Волоська, 8/5, корп.4',
+      rating: 4,
+      groups: ['PMBA', 'EMBA', 'EMBA(Agro)'],
+      distance: '350m',
+      _id: '5759719c216695a563a640e4'
+    },
+    {
+      name: 'Стратегічна ідея',
+      address: 'Київ, вул. Волоська, 8/5, корп.4',
+      rating: 5,
+      groups: ['PMBA', 'EMBA', 'EMBA(Agro)'],
+      distance: '350m',
+      _id: '57612088a9bba3104346dc84'
+    }
+  ]
+}
+
 angular
   .module('kkangst')
-  .controller('courseListCtrl', function () {
-    var vm = this
-    vm.courses = [
-      {
-        name: 'Маркетинг',
-        address: 'Київ, вул. Волоська, 8/5, корп.4',
-        rating: 4,
-        groups: ['PMBA', 'EMBA', 'EMBA(Agro)'],
-        distance: '350m',
-        _id: '5759719c216695a563a640e4'
-      },
-      {
-        name: 'Стратегічна ідея',
-        address: 'Київ, вул. Волоська, 8/5, корп.4',
-        rating: 5,
-        groups: ['PMBA', 'EMBA', 'EMBA(Agro)'],
-        distance: '350m',
-        _id: '57612088a9bba3104346dc84'
-      }
-    ]
-  })
+  .controller('mainCtrl', mainCtrl)
+  .controller('courseListCtrl', courseListCtrl)
