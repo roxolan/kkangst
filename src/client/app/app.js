41954a31c86905138a08511513f524d998d41378
Viktor Kotusenko <roxolanus@gmail.com>
2016-06-24 14:08:53 +0300

    update jade layout to display data hardcoded in NG controller

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -1,1 +1,34 @@
 // brand new angular app
+'use strict'
+
+angular.module('kkangst', [])
+
+var mainCtrl = function () {
+  var vm = this
+  vm.author = 'Вітя Котусенко'
+  vm.year = 1974
+}
+
+angular
+  .module('kkangst')
+  .controller('courseListCtrl', function () {
+    var vm = this
+    vm.courses = [
+      {
+        name: 'Маркетинг',
+        address: 'Київ, вул. Волоська, 8/5, корп.4',
+        rating: 4,
+        groups: ['PMBA', 'EMBA', 'EMBA(Agro)'],
+        distance: '350m',
+        _id: '5759719c216695a563a640e4'
+      },
+      {
+        name: 'Стратегічна ідея',
+        address: 'Київ, вул. Волоська, 8/5, корп.4',
+        rating: 5,
+        groups: ['PMBA', 'EMBA', 'EMBA(Agro)'],
+        distance: '350m',
+        _id: '57612088a9bba3104346dc84'
+      }
+    ]
+  })
