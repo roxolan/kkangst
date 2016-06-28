9f1d9973f5007bc21d132fa2bc0d7febf5a6c8be
Viktor Kotusenko <roxolanus@gmail.com>
2016-06-28 18:54:05 +0300

    create & add addHtmlLineBreaks filter

---
+++ src/client/app/common/filters/addHtmlLineBreaks.filter.js
@@ -0,0 +1,10 @@
+angular
+  .module('kkangst')
+  .filter('addHtmlLineBreaks', addHtmlLineBreaks)
+
+function addHtmlLineBreaks () {
+  return function (text) {
+    var output = text.replace(/\n/g, '<br/>')
+    return output
+  }
+}
