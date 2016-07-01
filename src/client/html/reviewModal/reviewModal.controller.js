05da26062c08b076125207839a0de73cca1a6c24
Viktor Kotusenko <roxolanus@gmail.com>
2016-07-01 20:54:26 +0300

    add onSubmit form handler

--- src/client/html/reviewModal/reviewModal.controller.js
+++ src/client/html/reviewModal/reviewModal.controller.js
@@ -6,6 +6,12 @@ reviewModalCtrl.$inject = ['$uibModalInstance', 'courseData']
 function reviewModalCtrl ($uibModalInstance, courseData) {
   var vm = this
   vm.courseData = courseData
+
+  vm.onSubmit = function () {
+    console.log(vm.formData)
+    return false
+  }
+
   vm.modal = {
     cancel: function () {
       $uibModalInstance.dismiss('cancel')
