5fdb30abdcf98d6b49e0975a157cdb6c3fb7cd57
Viktor Kotusenko <roxolanus@gmail.com>
2016-07-04 13:56:16 +0300

    validating a submitted password

--- src/api/models/users.js
+++ src/api/models/users.js
@@ -15,7 +15,12 @@ var userSchema = new mongoose.Schema({
   salt: String
 })
 
-userSchema.setPassword = function(password) {
+userSchema.methods.setPassword = function(password) {
   this.salt = crypto.randomBytes(16).toString('hex')
   this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex')
+}
+
+userSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex')
+  return this.hash === hash
 }
