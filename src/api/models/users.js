45aae5d6c850088dc2e68f08a559238bdda3a254
Viktor Kotusenko <roxolanus@gmail.com>
2016-07-04 16:16:12 +0300

    add .generateJwt() to userSchema.methods

--- src/api/models/users.js
+++ src/api/models/users.js
@@ -25,3 +25,18 @@ userSchema.methods.validPassword = function(password) {
   var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex')
   return this.hash === hash
 }
+
+userSchema.methods.generateJwt = function() {
+  var expiry = new Date()
+  expiry.setDate(expiry.getDate + 7)
+
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    name: this.name,
+    // include exp as Unix time in seconds:
+    exp: parseInt(expiry.getTime()/1000),
+    // send secret for hashing algorithm to use:
+  }, process.env.JWT_SECRET)
+  // do not keep your secret in the code!
+}
