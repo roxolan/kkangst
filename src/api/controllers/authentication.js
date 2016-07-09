9db82513d899488430d1129330a3e2c591a101ff
Viktor Kotusenko <roxolanus@gmail.com>
2016-07-09 23:58:45 +0300

    create the login controller

--- src/api/controllers/authentication.js
+++ src/api/controllers/authentication.js
@@ -35,3 +35,30 @@ module.exports.register = function(req, res) {
   })
 }
 
+module.exports.login = function(req, res) {
+  if(!req.body.email || !req.body.password) {
+    sendJSONresponse(res, 400, {
+      "message": "All fields required"
+    })
+    return
+  }
+  passport.authenticate('local', function(err, user, info){
+    var token
+
+    if (err) {
+      sendJSONresponse(res, 404, err)
+      return
+    }
+
+    if (user) {
+      token = user.generateJwt()
+      sendJSONresponse(res, 200, {
+        "token": token
+      })
+    } else {
+      // return info message (why authentication failed):
+      sendJSONresponse(res, 401, info)
+    }
+    // make req & res available to Passport:
+  })(req, res)
+}
